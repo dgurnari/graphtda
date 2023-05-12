5b2bd489479f40e8ca592301f6be6390752cd5a3
Davide Gurnari <45945769+dgurnari@users.noreply.github.com>
2023-05-12 17:45:08 +0200

    mock hack

--- docs/conf.py
+++ docs/conf.py
@@ -23,6 +23,24 @@ sys.path.insert(0, os.path.abspath('..'))
 
 import graphtda
 
+import mock
+ 
+MOCK_MODULES = [    
+    "numpy",
+    "pandas",
+    "networkx",
+    "numba",
+    "matplotlib",
+    "bokeh",
+    "sklearn",
+    "scipy",
+    "tqdm",
+    "pyrivet",
+    "ot"]
+
+for mod_name in MOCK_MODULES:
+    sys.modules[mod_name] = mock.Mock()
+
 # -- General configuration ---------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
